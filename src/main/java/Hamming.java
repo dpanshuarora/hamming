7d09b836584a8f6ea3b16af411cf83fce15361a7
Deepanshu <deepanshuarora@Deepanshus-MacBook-Air.local>
2018-05-26 18:18:35 +0530

    create hamming distance calculator

---
+++ src/main/java/Hamming.java
@@ -0,0 +1,27 @@
+public class Hamming {
+	private String leftStrand, rightStrand;
+
+  Hamming(String leftStrand, String rightStrand) {
+    this.leftStrand = leftStrand;
+    this.rightStrand = rightStrand;
+  }
+
+  public int getHammingDistance() {
+
+    checkValidStrands(leftStrand, rightStrand);
+
+    int hammingDistance = 0;
+    for(int i = 0; i < leftStrand.length(); i++) {
+      if(leftStrand.charAt(i) != rightStrand.charAt(i)) {
+        hammingDistance++;
+      }
+    }
+    return hammingDistance;
+  }
+
+  private void checkValidStrands(String leftStrand, String rightStrand) {
+    if(leftStrand.length() != rightStrand.length()) {
+      throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+    }
+  }
+}

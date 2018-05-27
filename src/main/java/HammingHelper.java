f6b449ccdb3f719bca8219b7da85c46b97041872
Deepanshu <deepanshuarora@Deepanshus-MacBook-Air.local>
2018-05-27 21:08:25 +0530

    fix indentation

--- src/main/java/HammingHelper.java
+++ src/main/java/HammingHelper.java
@@ -1,12 +1,12 @@
 public class HammingHelper {
 
-	public static void main(String[] args) {
-		Hamming one = new Hamming("GGACTGSA", "GGASAAALLA");
+  public static void main(String[] args) {
+    Hamming one = new Hamming("GGACTGSA", "GGASAAALLA");
 
-		try {
-			one.getHammingDistance();
-		} catch(IllegalArgumentException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+    try {
+      one.getHammingDistance();
+    } catch(IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
